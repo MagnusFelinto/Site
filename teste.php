b2af3d5fb79b3f331d533d4994ae160e85538f71
Magnus Felinto <magnusfelinto@gmail.com>
2022-06-12 12:08:29 -0300

    Primeira Versao

---
+++ teste.php
@@ -0,0 +1,43 @@
+<?Php
+
+namespace App;
+
+class Main
+{
+    private string $test;
+
+    public function __construct()
+    {
+        echo "Hello Main";
+    }
+    /**
+     * Just a teste
+     *
+     * @param  string $name
+     * @return void
+     */
+    public function teste(string $name): void
+    {
+    }
+
+    /**
+     * Get the value of test
+     */
+    public function getTest()
+    {
+        return $this->test;
+    }
+
+    /**
+     * Set the value of test
+     *
+     * @return  self
+     */
+    public function setTest($test)
+    {
+        $this->test = $test;
+
+        return $this;
+    }
+}
+(new main())->teste("Nome");
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.6.2',
+            'version' => '3.6.2.0',
+            'reference' => '5e4e71592f69da17871dba6e80dd51bce74a351a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
